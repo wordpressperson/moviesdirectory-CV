4b3120c9018268fc601df3d38f0c4ebdb7fa9ccb
Ayokunle <therealayokunle@gmail.com>
2023-08-11 23:43:24 +0100

    first commit 2

---
+++ build/precache-manifest.839daf98e495f1547347a679f7435363.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0fd0c81eeed3240e600",
+    "url": "/moviesdbapiproject/static/css/main.809c3738.chunk.css"
+  },
+  {
+    "revision": "f0fd0c81eeed3240e600",
+    "url": "/moviesdbapiproject/static/js/main.f0fd0c81.chunk.js"
+  },
+  {
+    "revision": "eeaef9b53be621bcf6b4",
+    "url": "/moviesdbapiproject/static/js/runtime~main.eeaef9b5.js"
+  },
+  {
+    "revision": "8254c8b40dfd6333fe28",
+    "url": "/moviesdbapiproject/static/js/2.8254c8b4.chunk.js"
+  },
+  {
+    "revision": "b24a081759c94b6176b7523bb5083982",
+    "url": "/moviesdbapiproject/static/media/olsen2.b24a0817.png"
+  },
+  {
+    "revision": "9c7897fa09b173d66743f82e497ccdcf",
+    "url": "/moviesdbapiproject/index.html"
+  }
+];
