2b2a216b156f4aba4f594583050107588a783634
Ayokunle <therealayokunle@gmail.com>
2023-08-12 02:48:05 +0100

    fkf

---
+++ build/precache-manifest.ed847c920d1f6b3797f68873685354a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54e24c9d8f0853ef97e6",
+    "url": "/static/css/main.809c3738.chunk.css"
+  },
+  {
+    "revision": "54e24c9d8f0853ef97e6",
+    "url": "/static/js/main.54e24c9d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "8254c8b40dfd6333fe28",
+    "url": "/static/js/2.8254c8b4.chunk.js"
+  },
+  {
+    "revision": "b24a081759c94b6176b7523bb5083982",
+    "url": "/static/media/olsen2.b24a0817.png"
+  },
+  {
+    "revision": "ba147fbbfad610b2c9cf4cc837776deb",
+    "url": "/index.html"
+  }
+];
